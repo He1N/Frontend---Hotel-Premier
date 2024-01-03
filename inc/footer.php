433690c01669c2d4b5e8473471cf44f1e2cb200a
Cristian Enrique Arredondo Vargas <110081529+He1N@users.noreply.github.com>
2024-01-03 12:10:00 -0500

    Version 1

---
+++ inc/footer.php
@@ -0,0 +1,40 @@
+<div class="container-fluid mt-5" style="background-color: #275466;">
+
+
+        <div class="row">
+          <div class="col-lg-4 p-4">
+            <a class="navbar-brand me-2" href="hotel_premier.html">
+              <img src="images/logo.png" alt="Logo Hotel Premier" class="img-fluid">
+          </a>
+          <br><br>
+          <a href="#" class="d-inline-block mb-2 text-white text-decoration-none">Nosotros</a><br>
+          <a href="#" class="d-inline-block mb-2 text-white text-decoration-none">Novedades</a><br>
+          <a href="#" class="d-inline-block mb-2 text-white text-decoration-none">Politica de privacidad</a><br>
+          <a href="#" class="d-inline-block text-white text-decoration-none mb-2"><i class="bi bi-book-half"></i> Libro de reclamaciones</a><br>
+            <br>
+          <h6 class="mb-3 text-white">Siguenos en</h6>
+            <a href="#" class="d-inline-block text-white text-decoration-none mb-2 mx-2"><i class="bi bi-facebook"></i></a>
+            <a href="#" class="d-inline-block text-white text-decoration-none mb-2 mx-2"><i class="bi bi-twitter"></i></a>
+            <a href="#" class="d-inline-block text-white text-decoration-none mb-2 mx-2"><i class="bi bi-youtube"></i></a>
+            <a href="#" class="d-inline-block text-white text-decoration-none mb-2 mx-2"><i class="bi bi-instagram"></i></a>
+            <a href="#" class="d-inline-block text-white text-decoration-none mb-2 mx-2"><i class="bi bi-linkedin"></i></a>
+          </div>
+          <div class="col-lg-4 p-4">
+              <h5 class="mb-3 text-white">Servicios</h5>
+              <h6 class=" mb-2 text-white text-decoration-none">Ascensor</h6>
+              <h6 class=" mb-2 text-white text-decoration-none">Secador de cabello</h6>
+              <h6 class=" mb-2 text-white text-decoration-none">Servicio a la habitación</h6>
+              
+          </div>
+          <div class="col-lg-4 p-4 ">
+              <h5 class="mb-3 text-white">Enlaces</h5>
+              <a href="#" class="d-inline-block mb-2 text-white text-decoration-none">Preguntas frecuentes</a><br>
+              <a href="#" class="d-inline-block mb-2 text-white text-decoration-none">¿Quienes somos?</a><br>
+              <a href="#" class="d-inline-block mb-2 text-white text-decoration-none">Términos y condiciones</a>
+          </div>
+        </div>
+      </div>
+
+      
+      <h6 class="   text-center  text-dark p-3 m-0" style="background-color: rgba(240, 224, 211, 1);">© 2023 Derechos Reservados Hotel Premier</h6>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
---
+++ inc/header.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-custom navbar-dark px-lg-3 py-lg-2 shadow-sm sticky-top">
+    <div class="container-fluid">
+      <a class="navbar-brand me-2" href="/Frontend/1/hotel_premier.php">
+        <img src="images/logo.png" alt="Logo Hotel Premier" class="img-fluid">
+    </a>
+      <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active me-3 " aria-current="page" href="/Frontend/1/hotel_premier.php">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link me-3" href="#">Habitaciones</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link me-3" href="#">Ubicación</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link me-3" href="#">Preguntas Frecuentes</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="languageDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="bi bi-globe"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end max-height-150 overflow-auto" aria-labelledby="languageDropdown">
+                <li><a class="dropdown-item" href="/Frontend/1/hotel_premier.php">Español</a></li>
+                <li><a class="dropdown-item" href="/Frontend/1/INGLES_hotel_premier.php">Ingles</a></li>
+            </ul>
+        </li>
+        
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="phoneDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="bi bi-phone"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end max-height-150 overflow-auto" aria-labelledby="phoneDropdown">
+         
+                <li><a class="dropdown-item" href="tel: +51 980920053">+51 980920053</a></li>
+                <li><a class="dropdown-item" href="tel: 052-246045">052-246045</a></li>
+                <li><a class="dropdown-item" href="tel: 052-415110">052-415110</a></li>
+            </ul>
+        </li>
+        
+        </ul>
+      </div>
+    </div>
+  </nav>
